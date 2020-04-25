02df4e541c48d43e5d3653155fbd3d6649bc8e0d
Александр Поликарпов <C:\Users\kgb\AppData\Roaming\The Bat!>
2020-04-25 20:21:31 +0100

    First commit test001

--- while loop.py
+++ while loop.py
@@ -1,11 +1,11 @@
 from random import random
-secret=int(random()*100)
+secret = int(random()*100)
 
-new=int(input("new "))
-while secret != new :
-    if secret < new :
-        print (" - ")
-    else :
-        print (" + ")
-    new=int(input("new "))
-print (new, "=", secret)
+new = int(input("new "))
+while secret != new:
+    if secret < new:
+        print(" - ")
+    else:
+        print(" + ")
+    new = int(input("new "))
+print(new, "=", secret)
